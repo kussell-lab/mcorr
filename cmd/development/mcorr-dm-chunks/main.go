c43935e3c71fad77ad7124a0115d399eda413a4c
Asher Preska Steinberg <asherps@gmail.com>
2020-12-05 16:16:21 -0500

    APS156 update for mcorr-dm-chunks with bounded parallelism; hopefully lighter memory usage

--- cmd/development/mcorr-dm-chunks/main.go
+++ cmd/development/mcorr-dm-chunks/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"github.com/sbinet/npyio"
 	"gonum.org/v1/gonum/mat"
@@ -25,7 +26,7 @@ func main() {
 	mpsoutdir := app.Arg("mcorr-pair-sync-dir", "directory containing mcorr-pair-sync output as .csv files").Required().String()
 	strains := app.Arg("strain-list", "list of all strains").Required().String()
 	out := app.Arg("distmatrix", "prefix for distance matrix output as .npy file").Required().String()
-	ncpu := app.Flag("num-cpu", "Number of CPUs (default: using all available cores)").Default("0").Int()
+	ncpu := app.Flag("num-workers", "Number of files read at a time (default: 20)").Default("20").Int()
 	showProgress := app.Flag("show-progress", "Show progress").Default("false").Bool()
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
@@ -70,35 +71,7 @@ func main() {
 		defer bar.Finish()
 	}
 
-	var files []string
-	//find dem csv files
-	err = filepath.Walk(*mpsoutdir, func(path string, info os.FileInfo, err error) error {
-		if filepath.Ext(path) == ".csv" {
-			files = append(files, path)
-		}
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	//dmMap := getDistances(strainMap, files...)
-
-	var wg sync.WaitGroup
-	var m sync.Mutex
-	//create a map to store distance matrix values in
-	//dmMap := make(map[pos_key]float64)
-	//dmMap := make(map[string]float64)
-	size := len(strainMap)
-	distmatrix := mat.NewDense(size, size, nil)
-
-	for i := 1; i < len(files); i++ {
-		wg.Add(1)
-		//go csvtomap(i, &wg, &m, files[i], strainMap, dmMap)
-		go csvtodistmatrix(i, &wg, &m, files[i], strainMap, distmatrix)
-	}
-
-	wg.Wait()
+	dmMap, err := mapAll(*mpsoutdir, strainMap, *ncpu)
 
 	res, err := os.Create(outFile)
 	if err != nil {
@@ -106,27 +79,28 @@ func main() {
 	}
 	defer res.Close()
 
-	//size := len(strainMap)
-	//distmatrix := mat.NewDense(size, size, nil)
+	size := len(strainMap)
+	distmatrix := mat.NewDense(size, size, nil)
 	//dims := size*size
 	//count := 0
-	//fmt.Printf("creating matrix ...")
+	fmt.Printf("creating matrix ...")
 	//loop over all rows in the matrix
 	//set starting column for each row
-	//col_init := 1
-	//for i := 0; i < size; i++ {
-	//	//loop over the columns
-	//	for j := col_init; j < size; j++ {
-	//		//get the dist
-	//		dmkey := strconv.Itoa(i)+strconv.Itoa(j)
-	//		//dist := dmMap[pos_key{i, j}]
-	//		dist := dmMap[dmkey]
-	//		distmatrix.Set(i, j, dist)
-	//		distmatrix.Set(j, i, dist)
-	//	}
-	//	//iterate the starting column
-	//	col_init++
-	//}
+	col_init := 1
+	for i := 0; i < size; i++ {
+		//loop over the columns
+		for j := col_init; j < size; j++ {
+			//get the dist
+			//dmkey := strconv.Itoa(i)+strconv.Itoa(j)
+			dist := dmMap[pos_key{i, j}]
+			//dist := dmMap[dmkey]
+			distmatrix.Set(i, j, dist)
+			distmatrix.Set(j, i, dist)
+		}
+		//iterate the starting column
+		col_init++
+	}
+
 	fmt.Printf("writing matrix ...")
 	err = npyio.Write(res, distmatrix)
 	duration := time.Since(start)
@@ -142,148 +116,69 @@ func getRecord(record []string) (strain1, strain2 string, dist float64) {
 
 }
 
-//channel to pump distance matrix into
-type dmResults struct {
-	pos_i int
-	pos_j int
-	dist  float64
-}
-
-//write the distance matrix to a npy file
-func WriteDM(dmChan chan dmResults, strainMap map[string]int, outFile string) {
-	size := len(strainMap)
-	distmatrix := mat.NewDense(size, size, nil)
-	for dm := range dmChan {
-		distmatrix.Set(dm.pos_i, dm.pos_j, dm.dist)
-		distmatrix.Set(dm.pos_j, dm.pos_i, dm.dist)
-
-	}
-	f, err := os.Create(outFile)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	err = npyio.Write(f, distmatrix)
-}
-
-//another equally valid way to do this
-func getDistances(strainMap map[string]int, files ...string) map[pos_key]float64 {
-
-	var wg sync.WaitGroup
-	var m sync.Mutex
-
-	filesLength := len(files)
-	//contents := make(map[string][]byte, filesLength)
-	dmMap := make(map[pos_key]float64)
-	//basically the wait group will be released once we've gone through all the files
-	wg.Add(filesLength)
-
-	for _, file := range files {
-		go func(file string) {
-			f, err := os.Open(file)
-			defer f.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-			r := csv.NewReader(f)
-
-			if _, err := r.Read(); err != nil {
-				panic(err)
-			}
-			//initialize a map for this file
-			fileMap := make(map[pos_key]float64)
-			for {
-				record, err := r.Read()
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					log.Fatal(err)
-				}
-				if record[5] == "all" {
-					continue
-				} else {
-					strain1, strain2, dist := getRecord(record)
-					pos_i, _ := strainMap[strain1]
-					pos_j, _ := strainMap[strain2]
-					fileMap[pos_key{pos_i, pos_j}] = dist
-					fileMap[pos_key{pos_j, pos_i}] = dist
-				}
-
-			}
-			//f.Close()
-			//lock the map so only one thread at a time can add to the distance matrix map
-			m.Lock()
-			//add the values from the fileMap to the distance matrix map
-			for k, dist := range fileMap {
-				dmMap[k] = dist
-			}
-			m.Unlock()
-			wg.Done()
-		}(file)
-	}
-
-	wg.Wait()
-
-	return dmMap
-}
-
 //position key for distance matrix as a map
 type pos_key struct {
 	pos_i int
 	pos_j int
 }
 
-func csvtomap(id int, wg *sync.WaitGroup, m *sync.Mutex, file string, strainMap map[string]int, dmMap map[string]float64) {
+// walkFiles starts a goroutine to walk the directory tree at root and send the
+// path of each regular file on the string channel.  It sends the result of the
+// walk on the error channel.  If done is closed, walkFiles abandons its work.
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
+	errc := make(chan error, 1)
+	go func() {
+		// Close the paths channel after Walk returns.
+		defer close(paths)
+		// No select needed for this send, since errc is buffered.
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error { // HL
+			if filepath.Ext(path) == ".csv" {
+				if err != nil {
+					return err
+				}
+				if !info.Mode().IsRegular() {
+					return nil
+				}
+				select {
+				case paths <- path:
+				case <-done: // HL
+					return errors.New("walk canceled")
+				}
+			}
+			return nil
+		})
+	}()
+	return paths, errc
+}
+
+// A result is a map of the pairwise distances in the file
+type result struct {
+	path    string
+	fileMap map[pos_key]float64
+	err     error
+}
+
+// digester reads path names from paths and sends digests of the corresponding
+// files on c until either paths or done is closed.
+func digester(done <-chan struct{}, paths <-chan string, c chan<- result, strainMap map[string]int, id int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Printf("Worker %d starting\n", id)
-	f, err := os.Open(file)
-	defer f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	r := csv.NewReader(f)
-	//skip the header
-	if _, err := r.Read(); err != nil {
-		panic(err)
-	}
-	//initialize a map for this file
-	fileMap := make(map[pos_key]float64)
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
+	for path := range paths { // HLpaths
+		//data, err := ioutil.ReadFile(path)
+		fileMap, err := mapFile(path, strainMap)
+		select {
+		case c <- result{path, fileMap, err}:
+		case <-done:
+			return
 		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		if record[5] == "all" {
-			continue
-		} else {
-			strain1, strain2, dist := getRecord(record)
-			pos_i, _ := strainMap[strain1]
-			pos_j, _ := strainMap[strain2]
-			fileMap[pos_key{pos_i, pos_j}] = dist
-			fileMap[pos_key{pos_j, pos_i}] = dist
-		}
-
 	}
-	//lock the map so only one thread at a time can add to the distance matrix map
-	m.Lock()
-	//add the values from the fileMap to the distance matrix map
-	for k, dist := range fileMap {
-		//dmMap[k] = dist
-		dmkey := strconv.Itoa(k.pos_i) + strconv.Itoa(k.pos_j)
-		dmMap[dmkey] = dist
-	}
-	m.Unlock()
-
 	fmt.Printf("Worker %d done\n", id)
 }
 
-func csvtodistmatrix(id int, wg *sync.WaitGroup, m *sync.Mutex, file string, strainMap map[string]int, distmatrix *mat.Dense) {
-	defer wg.Done()
-	fmt.Printf("Worker %d starting\n", id)
+//mapFile maps distances to matrix positions for the file
+func mapFile(file string, strainMap map[string]int) (map[pos_key]float64, error) {
+
 	f, err := os.Open(file)
 	defer f.Close()
 	if err != nil {
@@ -315,15 +210,59 @@ func csvtodistmatrix(id int, wg *sync.WaitGroup, m *sync.Mutex, file string, str
 		}
 
 	}
-	//lock the map so only one thread at a time can add to the distance matrix map
-	m.Lock()
-	//add the values from the fileMap to the distance matrix map
-	for k, dist := range fileMap {
-		//dmMap[k] = dist
-		distmatrix.Set(k.pos_i, k.pos_j, dist)
-		distmatrix.Set(k.pos_j, k.pos_i, dist)
-	}
-	m.Unlock()
+	return fileMap, err
+}
 
-	fmt.Printf("Worker %d done\n", id)
+// mapAll reads all the files in the file tree rooted at root and returns a map
+// of all the files.  If the directory walk
+// fails or any read operation fails, mapAll returns an error.  In that case,
+// mapAll does not wait for inflight read operations to complete.
+func mapAll(root string, strainMap map[string]int, numDigesters int) (map[pos_key]float64, error) {
+	// MD5All closes the done channel when it returns; it may do so before
+	// receiving all the values from c and errc.
+	done := make(chan struct{})
+	defer close(done)
+
+	paths, errc := walkFiles(done, root)
+
+	// Start a fixed number of goroutines to read and digest files.
+	c := make(chan result) // HLc
+	var wg sync.WaitGroup
+	//const numDigesters = 20
+	//wg.Add(numDigesters)
+	//for i := 0; i < numDigesters; i++ {
+	//	go func() {
+	//		digester(done, paths, c, strainMap, i) // HLc
+	//		wg.Done()
+	//	}()
+	//}
+
+	for i := 0; i < numDigesters; i++ {
+		wg.Add(1)
+		go digester(done, paths, c, strainMap, i, &wg) // HLc
+	}
+	go func() {
+		wg.Wait()
+		close(c) // HLc
+	}()
+	// End of pipeline. it's business time
+
+	dmMap := make(map[pos_key]float64)
+	for r := range c {
+		if r.err != nil {
+			return nil, r.err
+		}
+		//m[r.path] = r.sum
+		//add the values from the fileMap to the distance matrix map
+		for k, dist := range r.fileMap {
+			dmMap[k] = dist
+			//dmkey := strconv.Itoa(k.pos_i) + strconv.Itoa(k.pos_j)
+			//dmMap[dmkey] = dist
+		}
+	}
+	// Check whether the Walk failed.
+	if err := <-errc; err != nil { // HLerrc
+		return nil, err
+	}
+	return dmMap, nil
 }
