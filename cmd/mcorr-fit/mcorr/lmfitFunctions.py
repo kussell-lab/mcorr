7dc7e1efac09ea1b51c1a703eba67669dbc92bb7
Asher Preska Steinberg <asherps@gmail.com>
2021-02-02 17:58:00 -0500

    APS164 commit for troubleshooting mcorr-fit; updates to match jupyter notebook APS164_mcorr_Fit_troubleshooting_APS3.ipynb

--- cmd/mcorr-fit/mcorr/lmfitFunctions.py
+++ cmd/mcorr-fit/mcorr/lmfitFunctions.py
@@ -57,20 +57,25 @@ def residual(pars, x, data=None):
 def perform_lmfit(x, y, d_sample):
     """perform least-squares minimization"""
     pfit = Parameters()
-    pfit.add(name="phi_s", vary=True, min=0, max=1, value=1e-5)  ##is this maximum valid?
-    pfit.add(name="f", vary=True, min=0, max=3e5, value=1e3)
-    pfit.add(name="theta_s", vary=True, min=0, value=1e-5, max=d_sample) ##this maximum this valid?
-    pfit.add(name="theta_p", vary=True, min=0, value=1e-5)
+    pfit.add(name="phi_s", vary=True, min=0, value = 1e-5)
+    pfit.add(name="f", vary=True, min=0, max=3e5, value = 1e3)
+    pfit.add(name="theta_s", vary=True, min=0, value = 1e-5)
+    #pfit.add(name="theta_p", vary=True, min=0, value = 1e-4)
     ##define the fixed params
     pfit.add(name="w", value=2.0/3.0, vary=False)
     pfit.add(name="a", value=4.0/3.0, vary=False)
+    pfit.add(name="d_s", value=d_sample, vary=False)
     ##constrained params
-    pfit.add(name="c_s", expr="(phi_s*w*f)/(1+theta_s*a+phi_s*w*f)", min=0, max=1) #eq 21; range from 0 to 1 or eq 19 is unphysical
-    pfit.add(name="d_theta_p", expr="theta_p/(1+theta_p*a)", min=0) #eq 20 for theta_p -- should we have max 3/4?
+    pfit.add(name="c_s", expr="(phi_s*w*f)/(1+theta_s*a+phi_s*w*f)", min=0, max=1) #eq 21
+    #pfit.add(name="d_theta_p", expr="theta_p/(1+theta_p*a)", min=0) #eq 20 for theta_p
     pfit.add(name="d_theta_s", expr="theta_s/(1+theta_s*a)", min=0) #eq 20 for theta_s
-    pfit.add(name="d_s", value=d_sample, expr="c_s*d_theta_p+(1-c_s)*d_theta_s", vary=False) ##CONSTRAINING WITH EQ 19
-    ##adding in equation 27, but setting it to zero if theta_s is zero
-    pfit.add(name="phi_p", expr="(theta_p*phi_s)/theta_s if theta_s > 0 else 0") #eq. 27
+    #pfit.add(name="d_s", value=d_sample, expr="c_s*d_theta_p+(1-c_s)*d_theta_s", vary=False) ##CONSTRAINING WITH EQ 19
+    #pfit.add(name="c_s", expr="(d_s-d_theta_s)/(d_theta_p-d_theta_s)", min=0, max=1) ##CONSTRAINING WITH EQ 19
+    ##constraining with eq 26, but re-written ...
+    pfit.add(name="theta_p", expr="((1-c_s)*d_theta_s-d_s)/(a*(d_s-d_theta_s)+c_s*(d_theta_s*a-1))", min=0)
+    pfit.add(name="phi_p", expr="(theta_p*phi_s)/theta_s", min=0) #eq. 27
+    ##collect d_theta_p for later
+    pfit.add(name="d_theta_p", expr="theta_p/(1+theta_p*a)", vary=False) #eq 20 for theta_p
     myfit = Minimizer(residual, pfit,
                       fcn_args=(x,), fcn_kws={'data': y},
                       scale_covar=True)
